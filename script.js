934c58b9e9a9b5c8d04db9ae9807158512539f84
Relativelie <domasnaa@iMac.Dlink>
2021-07-31 23:11:46 +0300

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+const mortgageBlock = document.querySelector('.rectangularBlock');
+document.querySelector(".baseBlock").style.marginTop = '-900px !important'
+
+console.log("ffd")
+
+function showTypesOfMortgages() {
+    mortgageBlock.style.marginLeft = '-900px !important'
+}
+
+setTimeout(showTypesOfMortgages(), 3000)
