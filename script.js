bad85ecc45625015d8da4567f9cd8f16be823444
Relativelie <sopio1992@gmail.com>
2021-08-02 00:55:19 +0300

    add stars

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-const mortgageBlock = document.querySelector('.rectangularBlock');
-document.querySelector(".baseBlock").style.marginTop = '-900px !important'
+const mortgageBlock = document.querySelectorAll('.rectangularBlock');
 
-console.log("ffd")
 
+
+
+// show items one by one
 function showTypesOfMortgages() {
-    mortgageBlock.style.marginLeft = '-900px !important'
+    let i = 0;
+    let myInterval = setInterval(function () {
+        mortgageBlock[i++].style.marginLeft = '0px';
+        if (i == mortgageBlock.length) {
+            clearInterval(myInterval);
+        }
+    }, 400)
 }
 
-setTimeout(showTypesOfMortgages(), 3000)
+
+let observer = new IntersectionObserver(function (e) {
+    if (e[0].isIntersecting === true) {
+        showTypesOfMortgages()
+    }
+}, { threshold: [0.4] });
+
+observer.observe(document.querySelector(".baseBlock"));
+
+
+// STARTS
+const starsOnPage = document.querySelectorAll(".stars");
+
+let startsWidth = document.querySelector(".stars1").offsetWidth;
+let starsHeight = document.querySelector(".stars1").offsetHeight;
+
+
+// for left
+
+function starsShine() {
+    for (let i = 0; i < starsOnPage.length; i++) {
+        for (let stars = 10; stars > 0; stars--) {
+            let starLeftItem = starsOnPage[i].appendChild(document.createElement("div"));
+            starLeftItem.classList.add('starsItem');
+
+        }
+    }
+}
+
+starsShine()
+
+function changeStarPosition() {
+    let starsItem = document.querySelectorAll(".stars div");
+    console.log(starsItem)
+    for (let i = 0; i < starsItem.length; i++) {
+        starsItem[i].style.marginTop = Math.floor(Math.random() * 25 + starsHeight / 10) + "px";
+        starsItem[i].style.marginLeft = Math.floor(Math.random() * startsWidth / 4) + "px";
+        let starSize = Math.floor(1 + Math.random() * 4) + "px";
+        starsItem[i].style.height = starSize;
+        starsItem[i].style.width = starSize;
+    }
+}
+
+setInterval(changeStarPosition, 4000)
