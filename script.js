657927dac4592e3115f76c7b6882020205c125e3
Relativelie <sopio1992@gmail.com>
2021-08-02 21:37:28 +0300

    add mortgage statement and some script for calculator

--- script.js
+++ script.js
@@ -47,7 +47,6 @@ starsShine()
 
 function changeStarPosition() {
     let starsItem = document.querySelectorAll(".stars div");
-    console.log(starsItem)
     for (let i = 0; i < starsItem.length; i++) {
         starsItem[i].style.marginTop = Math.floor(Math.random() * 25 + starsHeight / 10) + "px";
         starsItem[i].style.marginLeft = Math.floor(Math.random() * startsWidth / 4) + "px";
@@ -58,3 +57,64 @@ function changeStarPosition() {
 }
 
 setInterval(changeStarPosition, 4000)
+
+
+// calculator
+
+const costRange = document.querySelector("#cost");
+const showCostValue = document.querySelector(".showCostValue");
+const initialFeeRange = document.querySelector("#initialFee");
+const showInitialFeeValue = document.querySelector(".showInitialFeeValue");
+const mortgageTerm = document.querySelector("#mortgageTerm");
+const showMortgageTermValue = document.querySelector(".showMortgageTermValue");
+
+
+
+
+costRange.addEventListener("mousedown", function () {
+    costRange.addEventListener("mousemove", function () {
+        let costValue = costRange.value;
+        showCostValue.value = new Intl.NumberFormat('ru-RU').format(costValue);
+        // initialFeeRange.max = costValue - 1000;
+
+        // 'en-IN'
+
+    })
+});
+
+initialFeeRange.addEventListener("mousedown", function () {
+    initialFeeRange.addEventListener("mousemove", function () {
+
+        let initialFeeValue = initialFeeRange.value;
+        showInitialFeeValue.value = new Intl.NumberFormat('ru-RU').format(initialFeeValue);
+
+        // if (showInitialFeeValue.value > costValue) {
+        //     showInitialFeeValue.value = costValue % 2;
+        // }
+    })
+});
+
+costRange.addEventListener("mousedown", function () {
+    mortgageTerm.addEventListener("mousemove", function () {
+        let mortgageTermValue = mortgageTerm.value;
+        showMortgageTermValue.value = new Intl.NumberFormat('ru-RU').format(mortgageTermValue);
+    })
+});
+
+const earthTax = 600;
+const marsTax = 450;
+const venusTax = 300;
+const universeTax = 0;
+let creditPercent
+
+if ((costRange.value - initialFeeRange.value) < 8000) {
+    creditPercent = 3.5
+}
+
+else if ((costRange.value - initialFeeRange.value) > 8000 & (costRange.value - initialFeeRange.value) < 13000) {
+    creditPercent = 3.5
+}
+
+else creditPercent = 2;
+
+let creditAmount = costRange.value - initialFeeRange.value;
