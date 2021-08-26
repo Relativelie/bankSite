30bc82fc7feed2a3d42503f00b9ba2c9e90c4774
Relativelie <sopio1992@gmail.com>
2021-08-26 20:29:49 +0300

    invest page(test)

--- investmentscript.js
+++ investmentscript.js
@@ -6,7 +6,6 @@ const testQuestions = [
     "How long have you been investing?",
     "Imagine the stock market has taken a dive. The Dow is down more than 15%. What do you do?",
     "What’s your main goal with your investments?",
-    "Of these investments, which looks the most enticing to you?",
     "Another hypothetical here: After a few years of saving, you’re ready to buy a new car. But the day before your trip to the dealership, your boss tells you you’re being laid off. What are you going to do?",
     "When it comes to saving, you:",
     "How old are you?",
@@ -19,12 +18,11 @@ const testAnswers = [
     ["I haven’t started yet", "For a while now", "A long, long time"],
     ["I have to sell it all", "I’m holding onto mostly everything, but selling a little", "I’m buying more. These stocks will rebound"],
     ["Staying in the black and losing nothing", "Making a little bit on top of what I have", "Turning a big profit"],
-    ["table", "table", "table"],
     ["Keep my old car", "Find a similar but more inexpensive car than the one I had picked out", "Buy the car I want"],
     ["Save a certain percentage every month, no matter what", "Save what you can each month, but not a set amount", "Hope to save something every month, but never seem to find any money left over"],
     ["45+", "30 to 45", "Under 30"],
     ["Less than 5 years", "5 to 10 years", "11+ years"],
-    ["Something like a new home, where the money will be spent quickly", "Something like college, where the money will be spent over a few years"],
+    ["Something like a new home, where the money will be spent quickly", "Something like college, where the money will be spent over a few years", "Something like retirement, where the money will be spread out over many years"],
     ["Less than 5 years", "5 to 10 years", "11+ years"]
 ];
 
@@ -32,43 +30,129 @@ const testAnswers = [
 const question = document.querySelector(".question");
 const answers = document.querySelectorAll(".answers");
 const continueBtn = document.querySelector(".continue");
-const backBtn = document.querySelector(".back");
+const backBtn = document.querySelector(".backBtn");
+const numberOfquestionSelector = document.querySelector(".nuberOfQuestion");
+const answerOne = document.querySelector("#answerOne");
+const answerTwo = document.querySelector("#answerTwo");
+const answerThree = document.querySelector("#answerThree");
 
-continueBtn.addEventListener("click", nextQuestion);
-backBtn.addEventListener("click", previousQuestion);
 
-let i = 0;
 
-function nextQuestion(e) {
-    e.preventDefault();
-    i++
-    question.textContent = testQuestions[i];
-    curentAnswers(i);
-}
 
-function previousQuestion(e) {
-    e.preventDefault();
-    i--;
-    question.textContent = testQuestions[i];
-    curentAnswers(i);
-}
+answerOne.addEventListener("click", enableContinueBtn);
+answerTwo.addEventListener("click", enableContinueBtn);
+answerThree.addEventListener("click", enableContinueBtn);
+continueBtn.addEventListener("click", onContinue);
+backBtn.addEventListener("click", OnBack);
 
-function curentAnswers(item) {
+
+
+// redraw current question
+function currentAnswers(item) {
+
     let answer = 0;
     while (answer < 3) {
-        console.log(1);
         answers[answer].textContent = testAnswers[item][answer];
         answer++;
     }
+    question.textContent = testQuestions[questionNumber];
+    numberOfquestionSelector.textContent = `${questionNumber + 1}/9`;
 }
 
-// displaing buttons when we located on first question
-function displayTestBtn(i) {
-    if (i===0) {
 
+//  enable continue button when answer is selected 
+continueBtn.disabled = "disabled";
+function enableContinueBtn() {
+    continueBtn.disabled = "";
+    continueBtn.classList.remove("disabledBtn");
+}
+
+
+// add checked on checkbox after transitioning to previous question
+let dictOfAnswers = {};
+let questionNumber = 0;
+
+
+function onContinue(e) {
+    e.preventDefault();
+    if (questionNumber < 8) {
+        dictOfAnswers[questionNumber] = document.querySelector('input[name="answer"]:checked').value;
+
+        questionNumber++;
+        currentAnswers(questionNumber);
+
+        if (questionNumber in dictOfAnswers) {
+            document.querySelector(`input[name="answer"][value="${dictOfAnswers[questionNumber]}"]`).checked = true;
+        }
+        else {
+            answerOne.checked = false;
+            answerTwo.checked = false;
+            answerThree.checked = false;
+            continueBtn.disabled = "disabled";
+            continueBtn.classList.add("disabledBtn");
+        }
+
+        backBtn.style.visibility = "visible";
     }
 
-    else {
+
+    else if (questionNumber === 8) {
+        let results = 0;
+        Object.values(dictOfAnswers).forEach(element => {
+            results += parseInt(element);
+            
+        });
+
+        // [1,8]
+        if (results >= 1 && results <= 8) {
+            showResult(0);
+        }
+
+
+        // [9,18]
+        else if (results >= 9 && results <= 18) {
+            showResult(1);
+        }
+
+        // [19,27]
+        else if (results >= 19 && results <= 27) {
+            showResult(2);
+        }
 
     }
+}
+
+
+function OnBack(e) {
+    e.preventDefault();
+
+    questionNumber--;
+
+    currentAnswers(questionNumber);
+
+    // hiding back button when we located on first question
+    if (questionNumber === 0) {
+        backBtn.style.visibility = "hidden";
+    }
+
+    document.querySelector(`input[name="answer"][value="${dictOfAnswers[questionNumber]}"]`).checked = true;
+
+    enableContinueBtn();
+}
+
+
+const personalResult = [
+    ["Balanced Funds", "Fixed-Income Funds"],
+    ["Specialty Funds", "Exchange Traded Funds(ETFs)"],
+    ["International / Global Funds", "Exchange Traded Funds(ETFs)"]
+];
+
+
+function showResult(s) {
+    document.querySelector(".resultOne h5").textContent = personalResult[s][0];
+    document.querySelector(".resultTwo h5").textContent = personalResult[s][1];
+    document.querySelector(".resultOne p").textContent = "Expected income: ";
+    document.querySelector(".resultTwo p").textContent = "Expected income: ";
+    document.querySelector(".results").style.display = "block";
+    document.querySelector(".testBlock").style.display = "none";
 }
